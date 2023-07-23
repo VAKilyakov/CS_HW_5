499fb93f69c08990d8756255606f84b15916c7a8
VAKilyakov <vakilyakov@bk.ru>
2023-07-23 13:37:51 +0300

    Task 2 is done v1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿/* Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
 */
 
@@ -54,3 +54,55 @@ PS D:\Home_Works\CS_HW_5> dotnet run
 Введите 3 элемент массива 234
 2
 */
+
+/* Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int InputNum(string message)
+{
+    
+    Console.Write(message + " ");
+    return Convert.ToInt32(Console.ReadLine());
+
+}
+
+int[] GenarateArray(int Length)
+{
+
+    int [] array = new int[Length];
+
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-1000,1000); //условное ограниечение для демонстрации работы программы
+    }
+
+    return array;
+
+}
+
+void PrintArrayAndResult(int[] array)
+{
+    Console.Write("[");
+
+    int Result = 0;
+
+    for(int i=0; i < array.Length; i++)
+    {
+       if ((i+1)%2 == 0) 
+       {
+
+            Result += array[i];
+
+       }
+    }
+
+    Console.Write(Result);
+}
+
+int Length = InputNum("Введите длину массива ");
+int[] array = GenarateArray(Length);
+PrintArrayAndResult(array);
